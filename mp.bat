92dc59f089cf1021f8fc199b4a9b37807353b1c7
Calibrato <142619733+Calibrato@users.noreply.github.com>
2023-08-18 19:39:53 +0200

    These files need eachother to run!!!
    cmdl 1.1

---
+++ mp.bat
@@ -0,0 +1,43 @@
+@echo off
+title About
+echo.
+echo.
+echo.
+echo Changelog:
+echo.
+echo v1.1.1
+echo.
+echo.
+echo Added text at the end of this page
+echo That's really all.
+echo.
+echo.
+echo.
+echo.
+echo About:
+echo.
+echo cmdl is CMD with various themes to improve the experience of the app.
+echo Included are 12 themes, along with an option to exit and this page, holding information about the app.
+echo cmdl doesn't change anything else apart from the themes, so all your favourite commands and batch files will run correctly.
+echo.
+echo.
+echo.
+echo FAQ:
+echo.
+echo Does cmdl support lowercase and uppercase letters on the selection screen?
+echo.
+echo Yes.
+echo.
+echo Does cmdl restrict commands or make it harder in any way to launch and create scripts?
+echo.
+echo No.
+echo.
+echo.
+echo.
+echo Report any bugs to @c4urpt on Discord
+echo.
+echo.
+echo.
+pause
+start cmdl
+exit
